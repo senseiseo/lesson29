c0542336ecc45d88e193c6b9f8d266974a230e91
ildar <1919mur@gmail.com>
2019-04-26 08:13:23 +0300

    //add barbers migration

--- app.rb
+++ app.rb
@@ -10,6 +10,10 @@ class Client < ActiveRecord::Base
 
 end	
 
+class Barber < ActiveRecord::Base
+
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
